f041f3ec5e82a677a400902ea5bce8e19d75a6fd
Millerspoil <aoditesi@yandex.ru>
2022-09-06 01:26:35 +0500

    1234

--- src/Animals.java
+++ src/Animals.java
@@ -1,2 +1,24 @@
 public class Animals {
+    private boolean iWantEat;   // тру если голодное
+    private boolean goodHealth;   // тру если здоровое
+    private boolean agression;  //тру тогда  укусит
+
+    public boolean getAgr(){
+        return agression;
+    }
+    public void setAgr(boolean x){
+        agression = x;
+    }
+    public boolean getEatStatus(){
+        return iWantEat;
+    }
+    public void setEatStatus(boolean x){
+        iWantEat = x;
+    }
+    public boolean getHealth(){
+        return goodHealth;
+    }
+    public void setHealth(boolean x){
+        goodHealth = x;
+    }
 }
